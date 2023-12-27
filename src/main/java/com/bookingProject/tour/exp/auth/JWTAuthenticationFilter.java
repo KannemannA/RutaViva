634ed3093ef4ff5b8396c56b975f110d666583af
alejo <alejokannemann@gmail.com>
2023-12-26 23:49:55 -0300

    test

--- src/main/java/com/bookingProject/tour/exp/auth/JWTAuthenticationFilter.java
+++ src/main/java/com/bookingProject/tour/exp/auth/JWTAuthenticationFilter.java
@@ -54,8 +54,4 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
             resolver.resolveException(request,response,null,ex);
         }
     }
-
-    public static void main(String[] args) {
-        System.out.println(System.getenv("JIO"));
-    }
 }
